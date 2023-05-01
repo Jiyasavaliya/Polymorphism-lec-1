2312214249120f18e5fde9e484d415f89f071653
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-05-01 14:45:56 +0530

    Update overloading.cpp

--- overloading.cpp
+++ overloading.cpp
@@ -1,43 +1,41 @@
 #include<iostream>
 using namespace std;
 
-class cricket{
-    
-	public:
-		
-	void gettotleovers(){
-		
-		cout<<"your totle overs is 10"<<endl;
-	}
-};
-
-class T20match : public cricket{
+class A{
 	
 	public:
-	
-	void gettotleovers(){
 		
-		cout<<"T20-your totle overs is 20"<<endl;
+		int a,b,c,d;
+	
+	void print(int a, int b){
+		 
+	cout<<a/b<<endl;
+			
+	}
+	void print(int c, int d, int e){
+			cout<<c-d-e<<endl;
+	}
+	
+};
+class B : public A{
+	
+		public:
+			int e,f,g,h,i,j,k,l,m;
+	
+	void print(int e, int f, int g, int h){
+			cout<<e*f*g*h<<endl;
+	}
+	void print(int i, int j, int k, int l, int m){
+			cout<<i+j+k+l+m<<endl;
 	}
 };
-
-class Testmatch : public cricket{
-	public:
+int main(){
 	
-	void gettotleovers(){
-		
-		cout<<"Testmatch-your totle overs is 30";
-	}
-};
-
-int main(){ 
-     
-cricket obj1;
-T20match obj2;
-Testmatch obj3;
-	 
+	B obj;
+	obj.A::print(10,2);
+	obj.A::print(11,3,3);
+	obj.B::print(2,2,2,2);
+	obj.B::print(2,4,4,2,2);  
 	
-	obj1.gettotleovers(); 
-	obj2.gettotleovers(); 
-	obj3.gettotleovers();
+	
 }
